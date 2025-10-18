aaf51b5aaa0e8f10d1f50722f1c6f1dd9f206cd9
Agustin Aranda <agustin.aranda2103@gmail.com>
2025-10-18 12:41:46 -0300

    Ùltimos cambios

--- AccessDataLogic/obj/Debug/net8.0/AccessDataLogic.AssemblyInfo.cs
+++ AccessDataLogic/obj/Debug/net8.0/AccessDataLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessDataLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e35fbdc99e78bbfa161b7051d823aaeb8f6d57b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e8c5394e8f219cac3f997a733d329edf711a3c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessDataLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessDataLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

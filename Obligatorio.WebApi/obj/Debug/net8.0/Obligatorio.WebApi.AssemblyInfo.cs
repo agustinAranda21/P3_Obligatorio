172da100b5aa12bd8361628a4f208ed88a78bc98
Agustin Aranda <agustin.aranda2103@gmail.com>
2025-10-22 19:12:12 -0300

    Cambio del nombre del primer controller en la Api

--- Obligatorio.WebApi/obj/Debug/net8.0/Obligatorio.WebApi.AssemblyInfo.cs
+++ Obligatorio.WebApi/obj/Debug/net8.0/Obligatorio.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Obligatorio.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5df1312e5d2f548da049e44d3b2cea338be37b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4ee1ac2750317beebcb743572850125ca063b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Obligatorio.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Obligatorio.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

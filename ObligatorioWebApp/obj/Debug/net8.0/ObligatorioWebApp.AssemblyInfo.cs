5601d8ab1e0377bf07a51abb0a7b5773b5165de4
Agustin Aranda <agustin.aranda2103@gmail.com>
2025-10-25 03:20:09 -0300

    Agregué más estilos con Boostrap para mejorar la UI

--- ObligatorioWebApp/obj/Debug/net8.0/ObligatorioWebApp.AssemblyInfo.cs
+++ ObligatorioWebApp/obj/Debug/net8.0/ObligatorioWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6abb7623d0ba61f70988f5971981237afa8b7aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75511316c980adc839d1ae012261cc7efce49262")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
